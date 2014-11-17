35d42418be07d5a248fb6f4b58f3be9927b08f70
poofytoo <victorhung92@gmail.com>
2014-11-16 16:16:20 -0800

    updated more projects

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,7 @@ $(function() {
   $('.projects-btn').on('click', function() {
     $(document).scrollTo($('.my-projects'), 500, {easing:'easeOutQuart'});
   })
+  $('.hacks-btn').on('click', function() {
+    $(document).scrollTo($('.my-hacks'), 500, {easing:'easeOutQuart'});
+  })
 });
